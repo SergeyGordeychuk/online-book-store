f86dce25226c95d07e14d38ed77d21e65f5cb8fe
Sergey gordeichuk <ya.gordeichuk@gmail.com>
2023-08-26 20:23:13 +0300

    fixed after maven

--- src/main/java/onlinebookstore/mapper/BookMapper.java
+++ src/main/java/onlinebookstore/mapper/BookMapper.java
@@ -5,11 +5,13 @@ import onlinebookstore.dto.BookDto;
 import onlinebookstore.dto.CreateBookRequestDto;
 import onlinebookstore.model.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
     BookDto toDto(Book book);
 
+    @Mapping(target = "id",ignore = true)
     Book toModel(CreateBookRequestDto requestDto);
 
 }
