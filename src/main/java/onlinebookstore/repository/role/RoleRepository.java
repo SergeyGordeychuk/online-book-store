6f0a8071d1168b6bd4bddf9af2c20c8940d555d3
Sergey Gordeychuk <93253163+SergeyGordeychuk@users.noreply.github.com>
2024-03-26 18:41:09 +0200

    Delete src/main/java/onlinebookstore/repository/role directory

--- src/main/java/onlinebookstore/repository/role/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package onlinebookstore.repository.role;
-
-import java.util.Optional;
-import onlinebookstore.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(Role.RoleName name);
-}
