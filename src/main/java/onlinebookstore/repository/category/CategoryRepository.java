b8b91db9d3091439b8cfe4252de2754f0455d6de
Sergey Gordeychuk <93253163+SergeyGordeychuk@users.noreply.github.com>
2024-03-26 18:40:59 +0200

    Delete src/main/java/onlinebookstore/repository/category directory

--- src/main/java/onlinebookstore/repository/category/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package onlinebookstore.repository.category;
-
-import onlinebookstore.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category,Long> {
-
-}
